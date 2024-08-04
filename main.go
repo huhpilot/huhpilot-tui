eb8fd6ddacd72542acf9611db2df841caed892fb
Artem Romanovch <artromone@mail.ru>
2024-08-04 23:13:51 +0300

    default list

--- main.go
+++ main.go
@@ -1,35 +1,71 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
-	"log"
+	"github.com/charmbracelet/lipgloss"
 )
 
-type model int
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
 
-func main() {
-	p := tea.NewProgram(model(5), tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
-		log.Fatal(err)
-	}
+type item struct {
+	title, desc string
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
+
+type model struct {
+	list list.Model
 }
 
 func (m model) Init() tea.Cmd {
 	return nil
 }
 
-func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := message.(type) {
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "esc", "ctrl+c":
+		if msg.String() == "ctrl+c" {
 			return m, tea.Quit
 		}
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
 	}
 
-	return m, nil
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
-	return ""
+	return docStyle.Render(m.list.View())
+}
+
+func main() {
+	items := []list.Item{
+		item{title: "Inbox", desc: "bucket for new tasks"},
+		item{title: "Notes", desc: "for references"},
+		item{title: "Delegate", desc: "you just wait this"},
+		item{title: "Calendar", desc: "time-bound"},
+		item{title: "Someday", desc: "one day later"},
+		item{title: "Project", desc: "complex task"},
+		item{title: "Backlog", desc: "do this this week"},
+		item{title: "Done", desc: "congrats"},
+	}
+
+	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
+	m.list.Title = "GTD"
+
+	p := tea.NewProgram(m, tea.WithAltScreen())
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
 }
